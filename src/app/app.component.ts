1d7abd08d296b670dbe10e2d8e0420dc559b9d00
peszabolcs <perjesidev@gmail.com>
2025-04-15 22:47:02 +0200

    make background style cascade global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,28 @@ import { MatIconModule } from '@angular/material/icon';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, MatIconModule],
-  template: `<router-outlet></router-outlet>`,
+  template: `
+    <div class="app-container h-full w-full relative">
+      <div
+        class="fixed inset-0 bg-gradient-to-br from-[#f8fbfc] via-[#edf7f9] to-[#f9f3e8]"
+      ></div>
+      <div class="bg-dots"></div>
+      <div class="relative h-full">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      :host {
+        display: block;
+        height: 100%;
+        width: 100%;
+      }
+      .app-container {
+        isolation: isolate;
+      }
+    `,
+  ],
 })
 export class AppComponent {}
